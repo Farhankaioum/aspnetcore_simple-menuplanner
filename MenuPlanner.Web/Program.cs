9e2bdf69c0e6533bc4c182c369343432da75923f
Farhan Kaioum <km1472233@gmail.com>
2020-11-08 23:24:39 +0600

    Serilog configuration added for logging

--- MenuPlanner.Web/Program.cs
+++ MenuPlanner.Web/Program.cs
@@ -1,6 +1,8 @@
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
 
 namespace Chaldal.MenuPlanner.Web
 {
@@ -8,12 +10,35 @@ namespace Chaldal.MenuPlanner.Web
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            Log.Logger = new LoggerConfiguration()
+                        .MinimumLevel.Debug()
+                        .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+                        .Enrich.FromLogContext()
+                        .WriteTo.RollingFile("Logs//menuplanner-log-{Date}.log")
+                        .CreateLogger();
+                try
+                {
+                    Log.Information("Application Starting up");
+                }
+                catch (Exception ex)
+                {
+                    Log.Fatal(ex, "An error occurred seeding the DB.");
+                }
+
+                finally
+                {
+                    Log.CloseAndFlush();
+                    host.Run();
+                }
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
